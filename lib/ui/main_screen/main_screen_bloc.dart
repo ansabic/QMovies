3c4c31e0a7c2bd0cdb1c5e178bb5e1ff07924ada
antonio <as@evolutio.hr>
2022-11-16 01:21:13 +0100

    list not working

--- lib/ui/main_screen/main_screen_bloc.dart
+++ lib/ui/main_screen/main_screen_bloc.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
 
 part 'main_screen_event.dart';
-
 part 'main_screen_state.dart';
 
 class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
